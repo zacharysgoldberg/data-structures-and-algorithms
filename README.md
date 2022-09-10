6b45fc359973aa98ee70ad94414902ce95302dbf
ZachGoldberg <zacharysgoldberg@outlook.com>
2022-09-09 19:00:15 -0700

    hackerrank 1 weeks problems, day 1 and 2

---
+++ README.md
@@ -0,0 +1,3 @@
+# Data-Structures-and-Algorithm problems
+
+Practice for DS&A's
--- dsa.py
+++ dsa.py
@@ -1,3 +1,4 @@
+from time import time
 from collections import Counter
 
 
@@ -29,15 +30,15 @@ res = Solution()
 # print(res.first_and_last(nums=nums, target=target))
 
 
-def majorityElement(nums):
+def majority_element(nums):
     cnt = Counter(nums).most_common(1)[0][0]
     print(cnt)
 
 
-# majorityElement([2, 2, 2, 1, 3, 2, 2, 3, 1])
+# majority_element([2, 2, 2, 1, 3, 2, 2, 3, 1])
 
 
-def plusMinus(arr):
+def plus_minus(arr):
     pos, neg, zero = 0, 0, 0
     for i in arr:
         if i > 0:
@@ -51,4 +52,65 @@ def plusMinus(arr):
     print(zero / len(arr))
 
 
-# plusMinus([-4, 3, -9, 0, 4, 1])
+# plus_minus([-4, 3, -9, 0, 4, 1])
+
+
+def mini_max_sum(arr):
+    arr.sort()
+    min = sum(arr[:-1])
+    max = sum(arr[1:])
+
+    print(min, max)
+
+
+# mini_max_sum([1, 2, 3, 4, 5])
+
+
+def time_conversion(s):
+    if s[0:2] == "12" and "PM" in s:
+        print(s.replace(s[-2::], ""))
+    elif s[0:2] == "12" and "AM" in s:
+        time = s.replace("AM", "").replace("12", "00")
+        print(time)
+    elif s[0:2] != "12" and 'AM' in s:
+        print(s.replace("AM", ""))
+    else:
+        t = str(int(s[0:2]) + 12)
+        time = s.replace(s[0:2], t).replace(s[-2::], "")
+        print(time)
+
+
+# time_conversion("06:40:03AM")
+
+def lonely_integer(a):
+    low = 0
+    high = len(a) - 1
+    while low <= high:
+        i = a.count(a[low])
+        j = a.count(a[high])
+        if i == 1:
+            return a[low]
+        elif j == 1:
+            return a[high]
+        else:
+            low += 1
+            high -= 1
+
+
+# print(lonely_integer([1, 2, 3, 4, 3, 2, 1]))
+
+
+def diagonal_difference(arr):
+    n = 3
+    d1 = 0
+    d2 = 0
+    for i in range(n):
+        d1 += arr[i][i]
+
+    for i in range(n):
+        d2 += arr[i][n - 1 - i]
+
+    return abs(d1 - d2)
+
+
+print(diagonal_difference([[11, 2, 4], [4, 5, 6], [10, 8, -12]]))
